fb5c91c3b99025aed7f59907120f5beb1165847f
Wlady <wladyspb85@gmail.com>
2026-05-07 12:09:20 +0200

    feat: add No More Care Studio, Terminal Stake, Increvolution; update IncreMemory desc

--- js/themes.js
+++ js/themes.js
@@ -44,9 +44,12 @@
   ];
 
   var PROJECTS_AI = [
-    { href: 'https://github.com/no-more-care/master-claw',       title: 'Master Claw',     i18n: 'proj_masterclaw_desc',  tags: ['AI', 'GameDev', 'GitHub'], perm: '-rwxr-xr-x', featured: true },
-    { href: 'https://www.moltbook.com/u/jinny_mnemonic',         title: 'Jinny Mnemonic',  i18n: 'proj_jinny_desc',       tags: ['AI', 'Agent'],             perm: '-rwxr-xr-x', featured: true },
-    { href: null,                                                 title: 'IncreMemory',     i18n: 'proj_increMemory_desc', tags: ['Game'],                    perm: '-rw-------', soon: true }
+    { href: 'https://github.com/no-more-care/master-claw',  title: 'Master Claw',         i18n: 'proj_masterclaw_desc',     tags: ['AI', 'GameDev', 'GitHub'], perm: '-rwxr-xr-x', featured: true },
+    { href: 'https://www.moltbook.com/u/jinny_mnemonic',    title: 'Jinny Mnemonic',      i18n: 'proj_jinny_desc',          tags: ['AI', 'Agent'],             perm: '-rwxr-xr-x', featured: true },
+    { href: 'https://nomorecare.gg',                        title: 'No More Care Studio', i18n: 'proj_nomorecare_desc',     tags: ['Studio', 'GameDev'],       perm: '-rwxr-xr-x', featured: true },
+    { href: 'https://nomorecare.gg',                        title: 'Terminal Stake',      i18n: 'proj_terminal_stake_desc', tags: ['Roguelike', 'GameDev'],    perm: '-rwxr-xr-x', featured: true },
+    { href: null,                                           title: 'IncreMemory',         i18n: 'proj_increMemory_desc',    tags: ['Auto-battler'],            perm: '-rw-------', soon: true },
+    { href: null,                                           title: 'Increvolution',       i18n: 'proj_increvolution_desc',  tags: ['Roguelike'],               perm: '-rw-------', soon: true }
   ];
 
   var PROJECTS_QSP = [
